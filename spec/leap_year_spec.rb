a1e48649ebc32e160bbc7c71e62b3a28fec3dbf5
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-03-25 13:06:44 +0000

    tests: add more thorough testing examples

--- spec/leap_year_spec.rb
+++ spec/leap_year_spec.rb
@@ -7,10 +7,19 @@ describe 'leap_year?' do
 
   it 'returns false if not divisible by 400' do
     expect(leap_year?(1900)).to eq false
+    expect(leap_year?(1700)).to eq false
   end
 
   it 'returns true if divisible by 4 but not 100' do
     expect(leap_year?(1988)).to eq true
     expect(leap_year?(2004)).to eq true
+    expect(leap_year?(2012)).to eq true
+  end
+
+  it 'returns false if not divisible by 4' do
+    expect(leap_year?(2009)).to eq false
+    expect(leap_year?(2011)).to eq false
+    expect(leap_year?(1970)).to eq false
+    expect(leap_year?(1500)).to eq false
   end
 end
