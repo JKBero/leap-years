304727e37ca47f8ff5e501fcfacc82664a8edef6
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-03-25 14:12:12 +0000

    feat: returns the closest leap year in the future

--- lib/closest_leap_year.rb
+++ lib/closest_leap_year.rb
@@ -1,3 +1,7 @@
 def closest_leap_year(year)
-  year
+  x = year
+  while !leap_year?(x)
+    x += 1
+  end
+  return x
 end
--- spec/closest_leap_year_spec.rb
+++ spec/closest_leap_year_spec.rb
@@ -5,4 +5,8 @@ describe 'closest_leap_year' do
     expect(closest_leap_year(2000)).to eq 2000
     expect(closest_leap_year(1988)).to eq 1988
   end
+
+  it 'returns the closest leap year in the future, if given a non-leap year' do
+    expect(closest_leap_year(2001)).to eq 2004
+  end
 end
